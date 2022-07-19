c4053468f235e31a7cb9c5affe3d28cbc1d3e0cd
navimov <navmovn@gmail.com>
2022-07-20 00:34:42 +0300

    Make Negative number

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function makeNegative(num) {
       function move (position, roll) {
         return position+=roll*2; 
       }
-      //Personalized Message
+      //Personalized Message variant 1
       function greet (name, owner) {
         if (name===owner) {
           return "Hello boss";
@@ -22,3 +22,24 @@ function makeNegative(num) {
           return "Hello guest";
         };
         }
+        //Personalized Message variant 2
+        function greet (name, owner) {
+            switch (name===owner) {
+                case true:
+                return "Hello boss";
+                default:
+                return "Hello guest";
+            };
+            }
+        // Keep Hydrated
+        function litres(time) {
+            return Math.floor(time/2);
+          }    
+        // Opposites Attract
+        function lovefunc(flower1, flower2){
+            if ((flower1+flower2) % 2===1)  {
+              return true;
+            } else  {
+              return false;
+            };
+            }  
