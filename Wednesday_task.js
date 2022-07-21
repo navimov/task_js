0d8a7ce9e1f1dac7184b5e47554973c12c40ebcd
navimov <navmovn@gmail.com>
2022-07-21 15:52:50 +0300

    find maxNum and minNUm

--- Wednesday_task.js
+++ Wednesday_task.js
@@ -2,5 +2,31 @@
 function stringToArray(string)  {
     return string.split(" ");  
   };
-  //
   
+  
+  
+  //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+  function DNAtoRNA(dna) {
+    return dna.replace(/T/g, "U");
+    }
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+    var min = function(list){
+        let lowestNum=0;
+         for (let i=0; i<list.length; i++)  {
+           if (lowestNum > list[i]) {
+             lowestNum=list[i];
+           ;}
+         };
+          return lowestNum;
+      }
+      
+      var max = function(list){
+        let bigestNum=0;
+         for (let i=0; i<list.length; i++)  {
+           if (bigestNum < list[i]) {
+             bigestNum=list[i];
+           ;}
+         };
+          return bigestNum;
+      }
