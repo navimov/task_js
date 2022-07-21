77decaa4ad3d8b5b7ddaca88b85e2fbdf86f607b
navimov <navmovn@gmail.com>
2022-07-22 01:30:43 +0300

    Finish

--- Wednesday_task.js
+++ Wednesday_task.js
@@ -1,9 +1,7 @@
-//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
     return string.split(" ");
 };
-
-
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
