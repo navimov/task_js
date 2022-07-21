d91435f52a633f79a3f9ee824a38071b7f329c13
navimov <navmovn@gmail.com>
2022-07-22 01:31:41 +0300

    Finish

--- Wednesday_task.js
+++ Wednesday_task.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
     return string.split(" ");
 };
