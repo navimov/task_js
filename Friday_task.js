619d62f6a22f95b80aa268fee6bedecd4945fde8
navimov <navmovn@gmail.com>
2022-07-23 17:10:48 +0300

    ffff

---
+++ Friday_task.js
@@ -0,0 +1,5 @@
+// TASK_#2
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+    return 2 * 3.141592653589793238 * circle.radius;
+    }
