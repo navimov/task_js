754145f3ad64d2400e40099b0874499d8ca0b375
navimov <navmovn@gmail.com>
2022-07-23 23:34:44 +0300

    ааа

--- Friday_task.js
+++ Friday_task.js
@@ -1,3 +1,11 @@
+// TASK_#1
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min.apply( null, args);
+    };
+    
+  }
 // TASK_#2
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 function circleCircumference(circle) {
